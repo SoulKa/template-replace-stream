86299ee0f49d14c36e5ed0f2bfb9994e9ecffb1a
SoulKa <louiswilke@gmx.de>
2024-05-26 17:36:17 +0200

    drastically improve performance by using Buffer.indexOf()

--- src/template-replace-stream.ts
+++ src/template-replace-stream.ts
@@ -6,15 +6,23 @@ import {Readable, Transform, TransformCallback, TransformOptions} from 'node:str
 export type TemplateReplaceStreamOptions = {
   /** Default: `false`. If true, the stream creates logs on debug level */
   log: boolean;
-  /** Default: `false`. If true, the stream throws an error when a variable is missing */
-  throwOnMissingVariable: boolean;
-  /** Default: `100`. The maximum length of a variable name including whitespaces around it */
+  /**
+   * Default: `false`. If true, the stream throws an error when a template variable has no
+   * replacement value
+   */
+  throwOnUnmatchedTemplate: boolean;
+  /**
+   * Default: `100`. The maximum length of a variable name between a start and end pattern including
+   * whitespaces around it. Any variable name longer than this length is ignored, i.e. the search
+   * for the end pattern canceled and the stream looks for the next start pattern.
+   * Note that a shorter length improves performance but may not find all variables.
+   */
   maxVariableNameLength: number;
-  /** Default: `'{{'`. The start pattern of a variable either as string or buffer */
+  /** Default: `'{{'`. The start pattern of a template string either as string or buffer */
   startPattern: string | Buffer;
-  /** Default: `'}}'`. The end pattern of a variable either as string or buffer */
+  /** Default: `'}}'`. The end pattern of a template string either as string or buffer */
   endPattern: string | Buffer;
-  /** The options for the lower level {@link Transform} stream. Do not replace transform or flush */
+  /** Any options for the lower level {@link Transform} stream. Do not replace transform or flush */
   streamOptions?: TransformOptions;
 }
 
@@ -26,14 +34,22 @@ export type VariableResolverFunction = (variable: string) => StringSource | unde
 /** A map or function that resolves variable names to their values */
 export type VariableResolver = Map<string, StringSource> | VariableResolverFunction;
 
+enum State {
+  SEARCHING_START_PATTERN,
+  PROCESSING_VARIABLE,
+  SEARCHING_END_PATTERN
+}
+
 const DEFAULT_OPTIONS: TemplateReplaceStreamOptions = {
   log: false,
-  throwOnMissingVariable: false,
+  throwOnUnmatchedTemplate: false,
   maxVariableNameLength: 100,
   startPattern: Buffer.from('{{', 'ascii'),
   endPattern: Buffer.from('}}', 'ascii'),
   streamOptions: undefined
 }
+
+const SPACE = ' '.charCodeAt(0);
 
 /**
  * A stream that replaces template variables in a stream with values from a map or resolver function.
@@ -41,13 +57,12 @@ const DEFAULT_OPTIONS: TemplateReplaceStreamOptions = {
 export class TemplateReplaceStream extends Transform {
 
   private _stack = Buffer.alloc(0);
-  private _isMatching = false;
+  private _state = State.SEARCHING_START_PATTERN;
   private _matchCount = 0;
-  private _variableName?: Buffer;
+  private _stackIndex = 0;
 
   private readonly _startPattern: Buffer;
   private readonly _endPattern: Buffer;
-  private readonly _maxFullPatternLength: number;
   private readonly _resolveVariable: VariableResolverFunction;
   private readonly _options: TemplateReplaceStreamOptions;
 
@@ -75,7 +90,6 @@ export class TemplateReplaceStream extends Transform {
     this._options = _options;
     this._startPattern = this.toBuffer(_options.startPattern);
     this._endPattern = this.toBuffer(_options.endPattern);
-    this._maxFullPatternLength = this._startPattern.length + _options.maxVariableNameLength + this._endPattern.length;
     this._resolveVariable = variables instanceof Map ? variables.get.bind(variables) : variables;
   }
 
@@ -91,27 +105,31 @@ export class TemplateReplaceStream extends Transform {
         this._stack = Buffer.concat([this._stack, chunk]);
       }
 
-      // process the stack until it is empty or too small to contain a full pattern
-      while (this._stack.length > 0) {
-        if (!this._isMatching) {
-          if (!this.findStartOfVariable()) break;
-        } else if (!this.findEndOfVariable()) {
-          break;
-        } else if (this._variableName !== undefined) {
-          const value = this.getValueOfVariable(this._variableName);
-          const index = this._startPattern.length + this._variableName.length + this._endPattern.length;
-          if (value) {
-            await this.writeToOutput(value); // replace the template string with the value
-            this._stack = this._stack.subarray(index); // discard the template string
-          } else {
-            this.releaseStack(index); // write the original template string
-          }
-          this._variableName = undefined;
-          this.changeState();
+      while (this._stackIndex < this._stack.length) {
+        switch (this._state) {
+          case State.SEARCHING_START_PATTERN:
+            this.findStartPattern();
+            this.releaseStack(this._stackIndex - this._matchCount);
+            break;
+          case State.PROCESSING_VARIABLE:
+            this.findVariableEnd();
+            break;
+          case State.SEARCHING_END_PATTERN:
+            if (this.findEndPattern()) {
+              const variableNameBuffer = this._stack.subarray(this._startPattern.length, this._stackIndex - this._endPattern.length);
+              const value = this.getValueOfVariable(variableNameBuffer);
+              if (value) {
+                await this.writeToOutput(value); // replace the template string with the value
+                this._stack = this._stack.subarray(this._stackIndex); // discard the template string
+              } else {
+                this.releaseStack(this._stackIndex); // write the original template string
+              }
+            }
+            break;
         }
       }
     } else {
-      if (this._options.throwOnMissingVariable) {
+      if (this._options.throwOnUnmatchedTemplate) {
         throw new Error('Cannot replace variables in non-string-link streams');
       } else if (this._options.log) {
         console.warn('Received non-buffer chunk. Will not modify it.');
@@ -128,40 +146,78 @@ export class TemplateReplaceStream extends Transform {
   }
 
   /**
-   * Finds the start of a variable in the stack.
-   *
-   * @returns True if the start of a variable was found, false otherwise.
+   * Stateful function to find the index of the start pattern in the stack. If the start pattern is
+   * found, the stack is cropped to the start pattern and the state is set to processing variable.
    */
-  private findStartOfVariable() {
-    const index = this.findIndex(this._startPattern);
-    if (index === -1) {
-      this.releaseStack(this._stack.length - this._matchCount);
-      return false;
-    } else {
-      this.releaseStack(index);
-      this.changeState();
-      return true;
+  private findStartPattern() {
+    if (this._matchCount === 0) {
+      if ((this._stackIndex = this._stack.indexOf(this._startPattern[0])) === -1) {
+        this._stackIndex = this._stack.length;
+        return; // no match found
+      }
+      this._matchCount++;
+      this._stackIndex++;
     }
+
+    // continue matching the start pattern
+    for (; this._matchCount < this._startPattern.length; this._matchCount++ & this._stackIndex++) {
+      if (this._stackIndex >= this._stack.length) return; // end of stack reached, need more data
+      if (this._stack[this._stackIndex] !== this._startPattern[this._matchCount]) {
+        this._matchCount = 0;
+        return; // no match
+      }
+    }
+    this._state = State.PROCESSING_VARIABLE;
   }
 
   /**
-   * Finds the end of a variable in the stack and
+   * Stateful function to find the end of a variable in the stack. If the end pattern is found, the
+   * state is set to searching end pattern. If the maximum variable name length is reached, or a start
+   * pattern symbol is found, the state is set to searching start pattern.
+   */
+  private findVariableEnd() {
+    for (; this._stackIndex < this._options.maxVariableNameLength + this._startPattern.length; this._stackIndex++) {
+      if (this._stackIndex >= this._stack.length) return; // end of stack reached, need more data
+      const char = this._stack[this._stackIndex];
+      if (char === this._endPattern[0]) {
+        this._state = State.SEARCHING_END_PATTERN;
+        this._matchCount = 1;
+        this._stackIndex++;
+        return;
+      } else if (char === this._startPattern[0]) {
+        this._state = State.SEARCHING_START_PATTERN;
+        this._matchCount = 1;
+        this._stackIndex++;
+        this.releaseStack(this._stackIndex - this._matchCount);
+        return;
+      }
+    }
+
+    // not found within the maximum length
+    if (this._options.throwOnUnmatchedTemplate) throw new Error('Variable name processing reached limit');
+    if (this._options.log) console.debug('Variable name processing reached limit, skipping');
+    this._state = State.SEARCHING_START_PATTERN;
+    this.releaseStack(this._stackIndex);
+  }
+
+  /**
+   * Finds the end of a variable in the stack
    *
    * @returns True if the end of a variable was found, false otherwise. Note that there can still be
    * a match when continuing the search with the next chunk.
    */
-  private findEndOfVariable() {
-    const index = this.findIndex(this._endPattern, this._startPattern.length, this._maxFullPatternLength - this._startPattern.length);
-    if (index === -1) {
-      if (this._stack.length >= this._maxFullPatternLength) {
-        this.releaseStack(this._maxFullPatternLength);
-        this.changeState();
+  private findEndPattern() {
+    for (; this._matchCount < this._endPattern.length; this._matchCount++ & this._stackIndex++) {
+      if (this._stackIndex >= this._stack.length) return false; // end of stack reached, need more data
+      if (this._stack[this._stackIndex] !== this._endPattern[this._matchCount]) {
+        this.releaseStack(this._stackIndex);
+        this._matchCount = 0;
+        this._state = State.SEARCHING_START_PATTERN;
+        return false; // no match
       }
-      return false;
-    } else {
-      this._variableName = this._stack.subarray(this._startPattern.length, index);
-      return true;
     }
+    this._state = State.SEARCHING_START_PATTERN;
+    return true; // match found
   }
 
   /**
@@ -179,37 +235,9 @@ export class TemplateReplaceStream extends Transform {
       this.push(this._stack.subarray(0, index));
       this._stack = this._stack.subarray(index);
     }
+    this._stackIndex -= index;
   }
 
-  /**
-   * Stateful function to find the index of a pattern in the stack. The function can
-   * search over multiple chunks and continues incomplete matches in the next chunk.
-   *
-   * @param pattern The pattern to search for
-   * @param offset The offset to start searching in the buffer
-   * @param maxLength The maximum length to search in the buffer
-   */
-  private findIndex(pattern: Buffer, offset = 0, maxLength = 0) {
-    const maxIndex = maxLength > 0 ? Math.min(this._stack.length, offset + this._matchCount + maxLength) : this._stack.length;
-    for (let index = offset + this._matchCount; index < maxIndex; index++) {
-      if (this._stack[index] === pattern[this._matchCount]) {
-        if (++this._matchCount === pattern.length) {
-          return index - pattern.length + 1;
-        }
-      } else {
-        this._matchCount = 0;
-      }
-    }
-    return -1;
-  }
-
-  /**
-   * Changes the state of the pattern matching and resets the match count.
-   */
-  private changeState() {
-    this._isMatching = !this._isMatching;
-    this._matchCount = 0;
-  }
 
   /**
    * Gets the value of a variable from the map by its name.
@@ -221,7 +249,7 @@ export class TemplateReplaceStream extends Transform {
     const variableName = variableBuffer.toString().trim();
     const value = this._resolveVariable(variableName);
     if (value === undefined) {
-      if (this._options.throwOnMissingVariable) {
+      if (this._options.throwOnUnmatchedTemplate) {
         throw new Error(`Variable "${variableName}" not found in the variable map`);
       } else if (this._options.log) {
         console.debug(`Unmatched variable "${variableName}"`);
