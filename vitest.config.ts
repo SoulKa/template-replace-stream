ee342b09acddac4d0e18d572e279d9431e9c5d25
SoulKa <louiswilke@gmx.de>
2025-08-13 21:02:41 +0200

    fix test config

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,10 +1,17 @@
+import path from "node:path";
 import { defineConfig } from "vitest/config";
+
+const ROOT_DIR = __dirname;
+const DIST_DIR = path.join(ROOT_DIR, "dist");
 
 export default defineConfig({
   test: {
-    include: ["tests/*.test.ts"],
     alias: {
-      "template-replace-stream": Boolean(process.env.CI) ? "dist" : __dirname,
+      "template-replace-stream":
+        process.env.npm_lifecycle_event === "test:build" ? DIST_DIR : ROOT_DIR,
+    },
+    coverage: {
+      include: ["index.ts"],
     },
   },
 });
