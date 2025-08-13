ea97ac3be788efc6443883a84e7fc1ee93db3a4b
SoulKa <louiswilke@gmx.de>
2025-08-13 21:32:48 +0200

    fix test coverage

--- vitest.config.ts
+++ vitest.config.ts
@@ -6,9 +6,12 @@ const DIST_DIR = path.join(ROOT_DIR, "dist");
 
 export default defineConfig({
   test: {
+    include: ["tests/**/*.test.ts"],
     alias: {
       "template-replace-stream":
-        process.env.npm_lifecycle_event === "test:build" ? DIST_DIR : ROOT_DIR,
+        process.env.npm_lifecycle_event === "test:build"
+          ? DIST_DIR
+          : path.join(ROOT_DIR, "index.ts"),
     },
     coverage: {
       include: ["index.ts"],
